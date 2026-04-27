9653581fce14926f131a5b16a20542a4670f52b7
Zahruddin Fanani <zahruddin.fanani17@gmail.com>
2026-04-27 19:39:55 +0700

    megedit nama

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Halo, Saya Zahruddin</h1>
+      <h1 className="text-4xl font-bold">Halo, Saya Zahruddin Fanani</h1>
       <p className="mt-4 text-xl text-gray-600">
         Sedang membangun seefan.my.id dengan Next.js
       </p>
